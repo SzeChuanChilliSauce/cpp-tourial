006c4751eb60e6a91f9d6ab02fa22a989347fdaf
chensq <chensiqi_scdy@163.com>
2020-04-17 09:47:46 +0800

    C++引用

--- day03-01reference/main.cpp
+++ day03-01reference/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <cmath>
+
+struct A {
+    int data;
+    int& foo() { return data; }
+};
+
+int& bar(int& a) { return a; }
+
+int& hum() {
+    int n = 123;
+    return n;
+}
+
 
 int main() {
+    A a = { 100 };
+    std::cout << a.data << std::endl; // 100
+    a.foo() = 200;
+    std::cout << a.data << std::endl; // 200
+
+    int x;
+    bar(x) = 1000;
+    std::cout << x << std::endl;
+
+//    int& r = hum();
+//    std::cout << r << std::endl;
+
 
     return 0;
 }
